200f3dcf05c721ed770539af246011a604c1c9c5
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-10-28 20:38:28 -0700

    added data

---
+++ src/assets/data.js
@@ -0,0 +1,278 @@
+export const dinos = [
+    {
+        id: 0,
+        name:"",
+        movies:["JP1", "JP2", "JP3", "JW", "JW2", "JW3"],
+        diet: "veggies/meat/omnivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 1,
+        name:"Allosaurus",
+        movies:["JW2", "JW3"],
+        diet: "Carnivore",
+        image: "url.blahblah" 
+    },
+    {
+        id: 2,
+        name:"Ankylosaurus",
+        movies:["JP3", "JW", "JW2", "JW3"],
+        diet: "Herbivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 3,
+        name:"Apatosaurus",
+        movies:["JW", "JW2", "JW3"],
+        diet: "Herbivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 4,
+        name:"Atrociraptor",
+        movies:["JW3"],
+        diet: "Carnivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 5,
+        name:"Baryonyx",
+        movies:["JW2", "JW3"],
+        diet: "Carnivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 6,
+        name:"Brachiosaurus",
+        movies:["JP1", "JP3", "JW2", "JW3"],
+        diet: "Herbivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 7,
+        name:"",
+        movies:["JP1", "JP2", "JP3", "JW", "JW2", "JW3"],
+        diet: "veggies/meat/omnivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 8,
+        name:"Carnotaurus",
+        movies:["JW2", "JW3"],
+        diet: "Carnivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 9,
+        name:"Ceratosaurus",
+        movies:["JP3"],
+        diet: "Carnivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 10,
+        name:"Compsognathus",
+        movies:[ "JP2", "JP3", "JW2", "JW3"],
+        diet: "Carnivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 11,
+        name:"Corythosaurus",
+        movies:[ "JP3"],
+        diet: "Herbivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 12,
+        name:"Dilophosaurus",
+        movies:["JP1", "JW3"],
+        diet: "Carnivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 13,
+        name:"Dimetrodon",
+        movies:["JW3"],
+        diet: "Carnivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 14,
+        name:"Dimorphodon",
+        movies:["JW", "JW2", "JW3"],
+        diet: "Carnivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 15,
+        name:"Dreadnoughtus",
+        movies:["JW3"],
+        diet: "Herbivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 16,
+        name:"Gallimimus",
+        movies:["JP1", "JP2", "JP3", "JW", "JW2", "JW3"],
+        diet: "Omnivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 17,
+        name:"Giganotosaurus",
+        movies:["JW3"],
+        diet: "Carnivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 18,
+        name:"Iguanodon",
+        movies:["JW3"],
+        diet: "Herbivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 19,
+        name:"Lystrosaurus",
+        movies:["JW3"],
+        diet: "Herbivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 20,
+        name:"Mamenchisaurus",
+        movies:["JP2"],
+        diet: "Herbivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 21,
+        name:"Microceratus",
+        movies:["JW3"],
+        diet: "Herbivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 22,
+        name:"Moros",
+        movies:["JW3"],
+        diet: "Carnivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 23,
+        name:"Mosasaurus",
+        movies:["JW", "JW2", "JW3"],
+        diet: "Carnivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 24,
+        name:"Oviraptor",
+        movies:["JW3"],
+        diet: "Omnivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 25,
+        name:"Nasutoceratops",
+        movies:["JW3"],
+        diet: "Herbivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 26,
+        name:"Parasaurolophus",
+        movies:["JP", "JP2", "JP3","JW", "JW2", "JW3"],
+        diet: "Herbivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 27,
+        name:"Pachycephalosaurus",
+        movies:["JP2", "JW"],
+        diet: "Carnivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 28,
+        name:"Pteranodon",
+        movies:["JP2", "JP3","JW", "JW2", "JW3"],
+        diet: "Carnivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 29,
+        name:"Pyroraptor",
+        movies:["JW3"],
+        diet: "Carnivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 30,
+        name:"Quetzalcoatlus",
+        movies:["JW3"],
+        diet: "Carnivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 31,
+        name:"Sinoceratops",
+        movies:["JW2", "JW3"],
+        diet: "Herbivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 32,
+        name:"Spinosaurus",
+        movies:["JP3"],
+        diet: "Carnivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 33,
+        name:"Stegosaurus",
+        movies:["JP2", "JP3", "JW", "JW2", "JW3"],
+        diet: "Herbivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 34,
+        name:"Stygimoloch",
+        movies:[ "JW2", "JW3"],
+        diet: "Herbivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 35,
+        name:"Velociraptor",
+        movies:["JP1", "JP2", "JP3", "JW", "JW2", "JW3"],
+        diet: "Carnivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 36,
+        name:"Therizinosaurus",
+        movies:["JW3"],
+        diet: "Herbivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 37,
+        name:"Triceratops",
+        movies:["JP1", "JP2", "JP3", "JW", "JW2", "JW3"],
+        diet: "veggies/meat/omnivore",
+        image: "url.blahblah"
+    },
+    {
+        id: 38,
+        name:"Tyrannosaurus rex",
+        movies:["JP1", "JP2", "JP3", "JW", "JW2", "JW3"],
+        diet: "Carnivore",
+        image: "url.blahblah"
+    },
+
+
+
+];
